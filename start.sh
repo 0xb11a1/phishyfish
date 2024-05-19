36ab483068965e70eb33c713da61c7485b926344
theuser <32736765+0xb11a1@users.noreply.github.com>
2024-05-19 19:24:11 +0400

    adding command line arguments

--- start.sh
+++ start.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 
-echo "[+] crating docker network if not exit"
-docker network create reverse_proxy_phishyfish
 
-if [[ -z $1 ]];
+# API_KEY=`head -c 16 /dev/random | xxd -p  | tr -d "\n"`
+# SLACK_URL=``
+
+usage() { 
+    RED='\033[0;31m'
+    Blue='\033[0;34m'
+    NC='\033[0m' 
+  echo -e "${RED}PhishyFish${NC} ${Blue}><(((º> ${NC} \nUsage: $0 [ -p password ] [ -s slack_webhookURL ] [-d domain_for_SSL_generation]" 
+}
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+
+while getopts "p:s:d:h:" flag
+do
+            case "${flag}" in
+                p) password=${OPTARG};;
+                s) slack_URL=${OPTARG};;
+                d) website_domain=${OPTARG};;
+                :) exit_abnormal;;
+                *) exit_abnormal;;
+            esac
+done
+
+echo "[+] Generating the config files"
+cat api/example.config.json | sed -e "s/SLACK_URL/$slack_URL/g" -e "s/API_KEY_PLACEHOLDER/$password/g" > api/config.json
+cp leet_frontend/example.env.local leet_frontend/.env.local
+cp user_frontend/example.env.local user_frontend/.env.local
+
+# echo using $password $slack_URL $website_domain
+
+if [[ $website_domain -eq "" ]];
 then 
-    echo "[+] using localhostdomain"
+    echo "[+] using localhost domain"
     curr_domain=localhost
 else
-    echo "[+] using $1 domain"
-    curr_domain=$1
+    echo "[+] using $website_domain domain"
+    curr_domain=$website_domain
 fi
 
 cat ./caddy/Caddyfile_template | sed "s/DOMAIN_PLACEHOLDER/$curr_domain/g" > ./caddy/Caddyfile
 
+
+echo "[+] crating docker network if not exit"
+docker network create reverse_proxy_phishyfish
 echo "[+] Starting the docker"
 docker-compose up --build --remove-orphans
